627c2f53080823507a068fb8029ee76a2b356ccb
Uday Kumar <84660525+ukreddyredhat@users.noreply.github.com>
2022-09-29 15:16:06 +0200

    Update gcpvm.tf

--- gcpvm.tf
+++ gcpvm.tf
@@ -2,7 +2,7 @@ resource "google_service_account" "default" {
   account_id   = "service_account_id"
   display_name = "Service Account"
 }
-
+#test1
 resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "e2-medium"
