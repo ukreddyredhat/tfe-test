c96b4c3519426736ed66debcdffbdab1229ac9ba
Uday Kumar <84660525+ukreddyredhat@users.noreply.github.com>
2022-10-31 09:42:57 +0100

    Update gcpvm.tf

--- gcpvm.tf
+++ gcpvm.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 variable "gcp_credentials" {
-  description = "GCP credentials needed by google provider"
+  description = "GCP credential needed by google provider"
 }
 
 variable "gcp_project" {
